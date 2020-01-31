c9549eeabde822dfdd3594e1728d27978689b63e
Elizabeth Kanzler <ejckanzler@yahoo.com>
2020-01-31 12:35:39 -0800

    Trying to figure out what's wrong

--- index.php
+++ index.php
@@ -5,18 +5,12 @@
     error_reporting(E_ALL);
     require_once('vendor/autoload.php');
     $f3 = Base::instance();
-    $f3->route('GET|POST /',
-        function($f3) {
+    $f3->route('GET|POST /', function($f3) {
             f3->set('title', "Home");
-            if(isset($_SESSION['logged-in'])) {
-                echo Template::instance()->render('html/head.html');
-                echo Template::instance()->render('html/home.html');
-            }
-            else {
-                $f3->reroute("/login");
-            }
-        }
-    );
+            echo Template::instance()->render('html/head.html');
+            echo Template::instance()->render('html/home.html');
+            
+        });
     $f3->route('GET /login', function($f3) {
         f3->set('title', "Log In");
         echo Template::instance()->render('html/head.html');
