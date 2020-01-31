844864ce441e4475469dd50c3d353f9db3fde0db
Elizabeth Kanzler <ejckanzler@yahoo.com>
2020-01-31 11:05:02 -0800

    Fixing a typo

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     $f3 = Base::instance();
     $f3->route('GET /',
         function($f3) {
-            $f3->set('name','world');
             echo Template::instance()->render('html/head.html');
             echo Template::instance()->render('html/test-home.html');
         }
