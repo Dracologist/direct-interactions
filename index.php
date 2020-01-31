26919cadc559ee9c96dcd1fd79961f2187958dca
Elizabeth Kanzler <ejckanzler@yahoo.com>
2020-01-31 11:45:01 -0800

    Setting up the basics to test routing for login

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <?php 
+    session_start();
     require_once('vendor/autoload.php');
     $f3 = Base::instance();
     //please work
-    $f3->route('GET /',
+    $f3->route('GET|POST /',
         function($f3) {
+            f3->set('title', "Home");
             echo Template::instance()->render('html/head.html');
-            echo Template::instance()->render('html/test-home.html');
+            if ($_SESSION['logged-in'] != TRUE) {
+                $f3->reroute("/login");
+            }
+            echo Template::instance()->render('html/home.html');
         }
         );
+    $f3->route('GET /login', function($f3) {
+        f3->set('title', "Log In");
+        echo Template::instance()->render('html/head.html');
+        echo Template::instance()->render('html/login.html');
+    });
+    $f3->route('POST /login', function($f3) {
+        $_SESSION['logged-in'] = TRUE;
+        $f3->reroute("/");
+    });
     $f3->run();
 ?>
