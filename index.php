2e09908c4ef363e8062b3d6e6ec01fb0a08bf965
Elizabeth Kanzler <ejckanzler@yahoo.com>
2020-01-31 11:07:15 -0800

    fixing typos

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
     require_once('vendor/autoload.php');
     $f3 = Base::instance();
+    //please work
     $f3->route('GET /',
         function($f3) {
             echo Template::instance()->render('html/head.html');
