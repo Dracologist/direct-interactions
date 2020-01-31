29ead24bcc43b67d62aca3181c88c67f7d421556
Elizabeth Kanzler <ejckanzler@yahoo.com>
2020-01-31 10:54:35 -0800

    testing the routing framework

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <title>Direct Interactions Shift Manager</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    </head>
-    <body>
-    	<h1>Testing</h1>
-    	<?php
-    	echo "<p class='h1'>Testing Bootstrap</p>"
-        ?>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    </body>
-</html>
-
+<?php 
+    require_once('vendor/autoload.php');
+    $f3 = Base::instance();
+    $f3->route('GET /',
+        function($f3) {
+            $f3->set('name','world');
+            echo Template::instance()->render('htlm/head.html');
+            echo Template::instance()->render('htlm/test-home.html');
+        }
+        );
+    $f3->run();
+?>
